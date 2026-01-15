2c5b2b5788eefa860243e4a4b5bd3985525e6de1
dharanidharan2011 <seodm.archon@gmail.com>
2026-01-15 14:12:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
+from routes import main_routes
 
 def create_app():
     app = Flask(__name__)
     
-    @app.route("/")
-    def home():
-        return "Hello from Flask App!"
+    app.register_blueprint(main_routes)
 
     return app
